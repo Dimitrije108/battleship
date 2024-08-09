2112f519ce2b889b0b8b407ea65c1fd095996b38
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-08-09 18:19:55 +0200

    test: modify and add ship placement tests

--- src/logic/Gameboard.test.js
+++ src/logic/Gameboard.test.js
@@ -1,20 +1,54 @@
+import Gameboard from './Gameboard';
+
 test('ship placed', () => {
   const board = new Gameboard();
-  expect(board.placeShip(board.ships[0], 5, 5, vertical)).toBeTruthy();
+  expect(board.placeShip(board.ships[0], 5, 5, 'vert')).toBeTruthy();
 });
 
 test('ship out of bounds', () => {
   const board = new Gameboard();
-  expect(board.placeShip(board.ships[1], 3, 8, horizontal)).toBeFalsy();
+  expect(board.placeShip(board.ships[0], 10, 7, 'vert')).toBeFalsy();
+  expect(board.placeShip(board.ships[1], 8, 3, 'hor')).toBeFalsy();
 });
 
 test('ship overlap', () => {
   const board = new Gameboard();
-  board.placeShip(board.ships[0], 5, 5, vertical);
-  expect(board.placeShip(board.ships[1], 8, 5, horizontal)).toBeFalsy();
+  board.placeShip(board.ships[0], 5, 5, 'vert');
+  expect(board.placeShip(board.ships[1], 5, 9, 'hor')).toBeFalsy();
 });
 
-// receiveAttack - takes a pair of coordinates, determines whether or not the attack hit a ship and then
-// sends the ‘hit’ function to the correct ship, or records the coordinates of the missed shot.
+test('ship overlap 2', () => {
+  const board = new Gameboard();
+  board.placeShip(board.ships[0], 1, 1, 'vert');
+  board.placeShip(board.ships[1], 5, 1, 'hor');
+  board.placeShip(board.ships[2], 10, 2, 'vert');
+  board.placeShip(board.ships[3], 6, 6, 'hor');
+  expect(board.placeShip(board.ships[4], 10, 4, 'hor')).toBeFalsy();
+});
+
+test('find node', () => {
+  const board = new Gameboard();
+  expect(board.find(5, 5)).toMatchObject({ x: 5, y: 5 });
+  expect(board.find(8, 10)).toMatchObject({ x: 8, y: 10 });
+});
+
+// test('attack hit', () => {
+//   const board = new Gameboard();
+//   board.placeShip(board.ships[0], 1, 1, 'vert');
+//   board.receiveAttack(1, 5).toBeTruthy();
+// });
+
+// test('attack miss', () => {
+//   const board = new Gameboard();
+//   board.placeShip(board.ships[0], 1, 1, 'vert');
+//   board.receiveAttack(2, 1).toBeFalsy();
+// });
+
+// receiveAttack - either hit a ship or hit an empty cell
+// if ship record hit
+// if empty record it on gameboard
+// gameboard should report if all ships are sunk - allShipsSunk();
+
+// there's also an option for attacking an already attacked spot?
 
 // Gameboards should be able to report whether or not all of their ships have been sunk.
