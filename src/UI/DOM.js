149b08cea1aaba06f19bddd77568ef76e5c67dd8
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-08-19 12:58:19 +0200

    feat: implement reset ships functionality
    Reset ships button will remove the placed ships from the board and allow player to place them again.

--- src/UI/DOM.js
+++ src/UI/DOM.js
@@ -5,6 +5,7 @@ import {
   dispCompBoard,
   delBoard,
   dispShips,
+  delShips,
 } from './displayBoard';
 // PlayGame class takes in a player and initiates a game
 export default class PlayGame {
@@ -12,7 +13,6 @@ export default class PlayGame {
     this.player = new Player(player);
     this.comp = new Player('comp');
     this.initBoard();
-    this.initDraggable();
   }
 
   initBoard() {
@@ -20,13 +20,53 @@ export default class PlayGame {
     dispBoard(this.player.gameboard);
     dispShips(this.player.board.ships);
     dispCompBoard(this.comp.gameboard);
+    this.initDraggable();
+    this.initResetBtn();
+    this.changeShipDir();
   }
 
   initDraggable() {
     const board = document.querySelector('.board');
+    // const unplacedShips = document.querySelectorAll('.unplaced-ship');
+
+    // unplacedShips.forEach((ship) => {
+    //   ship.draggable = true;
+
+    //   ship.addEventListener('dragstart', (e) => {
+    //     const shipName = ship.dataset.name;
+    //     // Find the dragged ship object
+    //     const dragged = this.player.board.ships.find(
+    //       (ship) => ship.name === shipName
+    //     );
+    //     e.dataTransfer.setData('text/plain', ship.name);
+    //     e.dataTransfer.effectAllowed = 'move';
+    //     e.target.classList.add('dragging');
+    //   });
+    // });
+
+    // board.addEventListener('dragover', (e) => {
+    //   e.preventDefault();
+    //   const draggable = document.querySelector('.unplaced-ship.dragging');
+    //   // Prevent placed item being draggable issue
+    //   if (!draggable) {
+    //     e.dataTransfer.dropEffect = 'none';
+    //     return;
+    //   }
+    //   e.dataTransfer.dropEffect = 'move';
+    //   e.target.classList.add('highlight');
+    // });
+
+    // board.addEventListener('dragleave', (e) => {
+    //   e.preventDefault();
+    //   e.target.classList.remove('highlight');
+    // });
 
     board.addEventListener('drop', (e) => {
       e.preventDefault();
+      e.target.classList.remove('highlight');
+      // Prevent placed item being draggable issue
+      const draggable = document.querySelector('.unplaced-ship.dragging');
+      if (!draggable) return;
       const shipName = e.dataTransfer.getData('text/plain');
       const ship = this.player.board.ships.find(
         (ship) => ship.name === shipName
@@ -42,6 +82,32 @@ export default class PlayGame {
       }
     });
   }
+
+  initResetBtn() {
+    const resetBtn = document.querySelector('.reset-ships-btn');
+
+    resetBtn.addEventListener('click', () => {
+      this.player.board.resetBoard();
+      delBoard();
+      delShips();
+      dispBoard(this.player.gameboard);
+      dispShips(this.player.board.ships);
+    });
+  }
+
+  changeShipDir() {
+    const changeDirBtn = document.querySelector('.change-dir-btn');
+
+    changeDirBtn.addEventListener('click', () => {
+      const unplacedShips = document.querySelectorAll('.unplaced-ship');
+      unplacedShips.forEach((ship) => {
+        const dir = ship.style.flexDirection || 'row';
+        ship.style.flexDirection = dir === 'row' ? 'column' : 'row';
+      });
+    });
+  }
+  // connect reset button with appropriate actions
+
   // there will be a place ship on the board event and method
   // but it will just be an event that triggers user input
   // and then it calls this.player.board.placeShip()
