22c8f1deff2c55ccb60bb3a65e7edf67ed162994
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-08-21 18:49:11 +0200

    feat: add computer class that generates random attack moves
    Computer class will generate an array of predetermined random attack moves to be executed during play phase.

--- src/logic/Player.js
+++ src/logic/Player.js
@@ -1,6 +1,6 @@
 import Gameboard from './Gameboard';
 
-export default class Player {
+export class Player {
   constructor(name) {
     this.name = name;
     this.board = new Gameboard();
@@ -8,5 +8,39 @@ export default class Player {
 
   get gameboard() {
     return this.board.board;
+  }
+}
+
+export class Computer {
+  constructor() {
+    this.name = 'Computer';
+    this.board = new Gameboard();
+    this.attackMoves = this.shuffleMoves(this.addMoves());
+  }
+
+  get gameboard() {
+    return this.board.board;
+  }
+  // Create an array of all possible attack moves
+  addMoves() {
+    const arr = [];
+    for (let i = 1; i <= 10; i++) {
+      for (let j = 1; j <= 10; j++) {
+        arr.push([j, i]);
+      }
+    }
+    return arr;
+  }
+  // Shuffle all computer attack moves (Fisher-Yates Shuffle)
+  shuffleMoves(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
+
+  attack() {
+    return this.attackMoves.shift();
   }
 }
