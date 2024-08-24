b2edd40d314ef9d7dec886703540341b8e0337ae
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-08-24 17:02:09 +0200

    feat: add restart game functionality upon game conclusion
    Play again button will appear once the game is over. It will start a new game by creating a new game class with same player name.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@
 import html from './index.html';
 import './style.css';
 import initNameModal from './UI/nameModal';
-import PlayGame from './UI/DOM';
+import initRestart from './UI/restartGame';
 
 initNameModal();
-
-// give out a prompt for a name / modal
-// create such PlayGame class instance
-// if restart is pressed then a new class instance
-// is created but with same player name
+initRestart();
